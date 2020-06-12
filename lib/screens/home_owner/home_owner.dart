3817df40ad64b24c4c1ad86e80de6ae563a72000
Vishisht-rao <32187985+Vishisht-rao@users.noreply.github.com>
2020-06-12 17:56:45 +0530

    login pages 17:56 friday

--- lib/screens/home_owner/home_owner.dart
+++ lib/screens/home_owner/home_owner.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:local_commodities/screens/services/auth.dart';
 
 class HomeOwner extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home Owner'),
+    return Scaffold(
+      backgroundColor:  Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Home Owner'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+            label: Text('Logout'),
+          ),
+        ],
+      ),
     );
   }
 }
