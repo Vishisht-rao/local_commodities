137ea49e4984c829f386c312d4994a6d49fc1156
Ashwa2001 <61651636+Ashwa2001@users.noreply.github.com>
2020-06-12 20:42:52 +0530

    Add files via upload
    item.dart is a class with diff features(name,brand,price)

---
+++ lib/screens/home_delivery/item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Item {
+  final String image;
+  final String brand;
+  final String name;
+  final int price;
+
+  Item({
+    @required this.image,
+    @required this.brand,
+    @required this.name,
+    @required this.price,
+    });
+}
